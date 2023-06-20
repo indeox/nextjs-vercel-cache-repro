638e7b81d8c39bc979f03e09b042a096480185cd
David Vella <davevella@gmail.com>
2023-06-20 13:05:38 +0100

    Add repro case

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
 /** Add your relevant code here for the issue to reproduce */
+const revalidate = 3600; // Cache for 1 hour
+
 export default function Home() {
-  return null
+  return (
+    <div style={{ padding: "50px" }}>
+      <p>👋 The time at page render is <strong>{new Date().toUTCString()}</strong></p>
+      <p><em>(Cache is set to {revalidate} seconds)</em></p>
+      <hr />
+      <br />
+      <br />
+      Visit{" "}
+      <a href="/api/revalidate" target="_blank">
+        /api/revalidate
+      </a>{" "}
+      to revalidate
+    </div>
+  );
 }
